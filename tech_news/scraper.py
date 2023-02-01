6b67c3be4ac6458decb58aa7f68aabcca3a300f2
lfpborba <luisfpb@icloud.com>
2023-02-01 12:11:27 -0300

    feat: scraper - fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,8 +1,21 @@
-#Plus Ultra
+import requests
+from time import sleep
 
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url, timeout=5, headers={"user-agent": "Fake user-agent"}
+        )
+        sleep(1)
+        response.raise_for_status()
+
+    except (requests.exceptions.HTTPError, requests.exceptions.Timeout):
+        return
+
+    else:
+        return response.text
 
 
 # Requisito 2
