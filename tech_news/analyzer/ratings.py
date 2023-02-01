90c5122c026231e07b98fa840b88bee9d5287e8b
lfpborba <luisfpb@icloud.com>
2023-02-01 16:52:33 -0300

    feat: top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,12 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    sorted_news = sorted(find_news(), key=lambda n: n['comments_count'],
+                         reverse=True)
+
+    return [(new["title"], new["url"]) for new in sorted_news[:5]]
 
 
 # Requisito 11
